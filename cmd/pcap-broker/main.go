4ffdb87d2f7816ba6425d179ea7f14cd1c37d60d
VaiTon <eyadlorenzo@gmail.com>
2023-12-18 10:51:54 +0100

    Use zerolog + intercept SIGINT (#2)
    * Use zerolog + intercept SIGINT
    * Use RFC3339 for time format and add -json flag to disable pretty printing
    * Added new flags to README and fixed issue when command exit was not successful

--- cmd/pcap-broker/main.go
+++ cmd/pcap-broker/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"os"
 	"os/exec"
+	"os/signal"
 	"time"
 
 	"github.com/google/shlex"
@@ -15,6 +16,9 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type PcapClient struct {
@@ -50,17 +54,34 @@ func lookupHostnameWithTimeout(addr net.Addr, timeout time.Duration) (string, st
 	return names[0], port, nil
 }
 
-func main() {
+var (
+	pcapCommand     = flag.String("cmd", "", "command to execute for pcap data (eg: tcpdump -i eth0 -n --immediate-mode -s 65535 -U -w -)")
+	listenAddress   = flag.String("listen", "", "listen address for pcap-over-ip (eg: localhost:4242)")
+	noReverseLookup = flag.Bool("n", false, "disable reverse lookup of connecting PCAP-over-IP client IP address")
+	debug           = flag.Bool("debug", false, "enable debug logging")
+	json            = flag.Bool("json", false, "enable json logging")
+)
 
-	pcapCommand := flag.String("cmd", "", "command to execute for pcap data (eg: tcpdump -i eth0 -n --immediate-mode -s 65535 -U -w -)")
-	listenAddress := flag.String("listen", "", "listen address for pcap-over-ip (eg: localhost:4242)")
-	noReverseLookup := flag.Bool("n", false, "disable reverse lookup of connecting PCAP-over-IP client IP address")
+func main() {
 	flag.Parse()
+
+	if !*json {
+		log.Logger = log.Output(zerolog.ConsoleWriter{
+			Out:        os.Stderr,
+			TimeFormat: time.RFC3339,
+		})
+	}
+
+	if *debug {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
 
 	if *pcapCommand == "" {
 		*pcapCommand = os.Getenv("PCAP_COMMAND")
 		if *pcapCommand == "" {
-			log.Fatalf("Error: PCAP_COMMAND or -cmd not set, see --help for usage")
+			log.Fatal().Msg("PCAP_COMMAND or -cmd not set, see --help for usage")
 		}
 	}
 
@@ -71,68 +92,83 @@ func main() {
 		}
 	}
 
-	log.Printf("config PCAP_COMMAND = %q", *pcapCommand)
-	log.Printf("config LISTEN_ADDRESS = %q", *listenAddress)
+	log.Debug().Str("pcapCommand", *pcapCommand).Send()
+	log.Debug().Str("listenAddress", *listenAddress).Send()
+
+	ctx, cancelFunc := signal.NotifyContext(context.Background(), os.Interrupt)
 
 	// Create connections to PcapClient map
-	var connMap = map[net.Conn]PcapClient{}
+	connMap := map[net.Conn]PcapClient{}
 
 	// Create a pipe for the command to write to, will be read by pcap.OpenOfflineFile
 	rStdout, wStdout, err := os.Pipe()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to create pipe")
 	}
-
-	// Important or these will eventually be garbage collected and the pipe will close
-	defer rStdout.Close()
-	defer wStdout.Close()
 
 	// Acquire pcap data
 	args, err := shlex.Split(*pcapCommand)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to parse PCAP_COMMAND")
 	}
-	cmd := exec.Command(args[0], args[1:]...)
-	log.Printf("cmd = %v", cmd.Args)
+	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
+	log.Debug().Strs("args", args).Send()
+
 	cmd.Stdout = wStdout
-	cmd.Stderr = os.Stderr
+	cmd.Stderr = log.Logger
 
 	err = cmd.Start()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to start command")
 	}
 
-	log.Printf("PID %v", cmd.Process.Pid)
+	log.Debug().Int("pid", cmd.Process.Pid).Msg("started process")
+
+	// close context on process exit
 	go func() {
 		err := cmd.Wait()
 		if err != nil {
-			log.Fatal("Process exited with error: ", err)
+			log.Fatal().Err(err).Msg("command exited with error")
 		}
-		log.Printf("process exited")
-		os.Exit(0)
+		cancelFunc()
 	}()
 
 	// Read from process stdout pipe
 	handle, err := pcap.OpenOfflineFile(rStdout)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to open pcap file")
 	}
 
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	packetSource.Lazy = true
 	packetSource.NoCopy = true
-	go processPackets(packetSource, connMap)
 
-	log.Printf("PCAP-over-IP server listening on %v", *listenAddress)
-	l, err := net.Listen("tcp", *listenAddress)
+	go processPackets(ctx, packetSource, connMap)
+
+	log.Info().Msgf("PCAP-over-IP server listening on %v. press CTRL-C to exit", *listenAddress)
+
+	config := net.ListenConfig{}
+	l, err := config.Listen(ctx, "tcp", *listenAddress)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("failed to listen")
 	}
+
+	// close listener on context cancel
+	go func() {
+		<-ctx.Done()
+		cancelFunc()
+		err := l.Close()
+		if err != nil {
+			log.Err(err).Msg("failed to close listener")
+		}
+	}()
 
 	for {
 		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
+		if err != nil && ctx.Err() == nil {
+			log.Fatal().Err(err).Msg("failed to accept connection")
+		} else if errors.Is(ctx.Err(), context.Canceled) {
+			break
 		}
 
 		if *noReverseLookup {
@@ -149,22 +185,56 @@ func main() {
 		writer := pcapgo.NewWriter(conn)
 
 		// Write pcap header
-		writer.WriteFileHeader(65535, handle.LinkType())
+		err = writer.WriteFileHeader(65535, handle.LinkType())
+		if err != nil {
+			log.Err(err).Msg("failed to write pcap header")
+			err := conn.Close()
+			if err != nil {
+				log.Err(err).Msg("failed to close connection")
+			}
+
+			continue
+		}
 
 		// add connection to map
 		connMap[conn] = PcapClient{writer: writer}
 	}
+
+	log.Info().Msg("PCAP-over-IP server exiting")
+
+	err = rStdout.Close()
+	if err != nil {
+		log.Err(err).Msg("failed to close read pipe")
+	}
+
+	err = wStdout.Close()
+	if err != nil {
+		log.Err(err).Msg("failed to close write pipe")
+	}
 }
 
-func processPackets(packetSource *gopacket.PacketSource, connMap map[net.Conn]PcapClient) {
+func processPackets(
+	ctx context.Context,
+	packetSource *gopacket.PacketSource,
+	connMap map[net.Conn]PcapClient,
+) {
 	for packet := range packetSource.Packets() {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+		}
+
 		for conn, stats := range connMap {
 			ci := packet.Metadata().CaptureInfo
 			err := stats.writer.WritePacket(ci, packet.Data())
 			if err != nil {
-				log.Println(err)
+				log.Err(err).Msg("failed to write packet to connection")
 				delete(connMap, conn)
-				conn.Close()
+				err := conn.Close()
+				if err != nil {
+					log.Err(err).Msg("failed to close connection")
+				}
 				continue
 			}
 			stats.totalPackets += 1
